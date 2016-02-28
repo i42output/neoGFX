c694b42f686c51ea06f8149fa93d8510a7d87367
Leigh Johnston <leigh@i42.co.uk>
2016-02-28 21:00:51 +0000

    VS2015

--- examples/neopede/src/main.cpp
+++ examples/neopede/src/main.cpp
@@ -1,8 +1,10 @@
-#include <neogfx/neogfx.hpp>
+/*#include <neogfx/neogfx.hpp>
 #include <neogfx/app.hpp>
 #include <neogfx/window.hpp>
 #include <neogfx/vertical_layout.hpp>
-#include <neogfx/sprite.hpp>
+#include <neogfx/sprite.hpp>*/
+
+#include <neogfx/numerical.hpp>
 
 namespace ng = neogfx;
 
@@ -22,14 +24,14 @@ template class ng::basic_vector<double, 3>;
 
 int main()
 {
-	ng::app app("neopede - neoGFX Sample Application");
-	ng::window window(800, 800);
-	ng::vertical_layout layout0(window);
+//	ng::app app("neopede - neoGFX Sample Application");
+//	ng::window window(800, 800);
+//	ng::vertical_layout layout0(window);
 
 	ng::vector3 v1(1.0, 2.0, 3.0);
 	ng::vector3 v2 = v1.yzy;
 	ng::vector2 v3 = v2.xx;
 	v2.yz = ng::vector2(42.0, 43.0);
 
-	return app.exec();
+//	return app.exec();
 }
