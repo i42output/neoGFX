a0cb84be8f31f307c7742c700018a4b0ff6aec90
Leigh Johnston <leigh@i42.co.uk>
2017-04-09 21:41:20 +0100

    CSS3 Parser

---
+++ include/neogfx/core/css.hpp
@@ -0,0 +1,130 @@
+// css.hpp
+/*
+  neogfx C++ GUI Library
+  Copyright(C) 2017 Leigh Johnston
+  
+  This program is free software: you can redistribute it and / or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <neogfx/neogfx.hpp>
+#include <neolib/variant.hpp>
+#include <neolib/vecarray.hpp>
+#include "geometry.hpp"
+#include "hsl_colour.hpp"
+#include "hsv_colour.hpp"
+#include "colour.hpp"
+
+namespace neogfx
+{
+	class css
+	{
+	public:
+		class selector
+		{
+		public:
+			typedef std::string element;
+			typedef boost::optional<element> optional_element;
+			enum type_e
+			{
+				Universal,
+				Type,
+				Descendant,
+				Child,
+				AdjacentSibling,
+				GeneralSibling,
+				Class,
+				ID,
+				SimpleAttribute,
+				ExactAttributeValue,
+				PartialAttributeValue,
+				BeginniingSubstringAttributeValue,
+				EndingSubstringAttributeValue,
+				ArbitrarySubstringAttributeValue,
+				LanguageAttribute
+			};
+			enum pseudo_class_e
+			{
+				// Structural
+				Empty,
+				FirstChild,
+				FirstOfType,
+				Lang,
+				LastChild,
+				LastOfType,
+				NthChild,
+				NthLastChild,
+				NthLastOfType,
+				NthOfType,
+				OnlyChild,
+				OnlyOfType,
+				Root,
+				// Negation
+				Not,
+				// Interaction
+				Active,
+				Checked,
+				Disabled,
+				Enabled,
+				Focus,
+				Hover,
+				Link,
+				Target,
+				Visited
+			};
+			typedef neolib::variant<std::string> pseudo_class_argument;
+			typedef neolib::variant<std::string, std::pair<pseudo_class_e, pseudo_class_argument>> argument;
+			typedef neolib::vecarray<argument, 4, -1> arguments_type;
+			typedef std::pair<type_e, arguments_type> value_type;
+		public:
+			selector(type_e aType, const arguments_type& aArguments = arguments_type());
+		public:
+			type_e type() const;
+			const arguments_type& arguments() const;
+		private:
+			type_e iType;
+			arguments_type iArguments;
+		};
+		class declaration
+		{
+		public:
+			declaration();
+		};
+		typedef std::vector<declaration> declaration_block;
+		typedef std::list<selector> selector_list;
+		typedef std::list<declaration_block> declaration_block_list;
+		typedef std::vector<std::pair<selector_list::const_iterator, declaration_block_list::const_iterator>> rule_list;
+	public:
+		class i_visitor
+		{
+		public:
+			virtual const std::string& element_name() = 0;
+			virtual const std::string& id() = 0;
+			virtual bool has_parent() = 0;
+			virtual i_visitor& parent() = 0;
+		public:
+			virtual void apply(const declaration_block& aDeclarations) = 0
+		};
+	public:
+		css(const std::string& aStyleSheet);
+		css(const std::istream& aStyleSheet);
+	public:
+		void accept(i_visitor& aVisitor) const;
+		const rule_list& rules() const;
+		std::string to_string() const;
+	private:
+		rule_list iRules;
+	};
+}
---
+++ src/core/css.cpp
@@ -0,0 +1,122 @@
+// css.cpp
+/*
+  neogfx C++ GUI Library
+  Copyright(C) 2017 Leigh Johnston
+  
+  This program is free software: you can redistribute it and / or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <neogfx/neogfx.hpp>
+#include <string>
+#include <neolib/vecarray.hpp>
+#include <neogfx/core/css.hpp>
+
+using namespace std::string_literals;
+
+namespace neogfx
+{
+	css::selector::selector(type_e aType, const arguments_type& aArguments = arguments_type()) :
+		iType(aType), iArguments(aArguments)
+	{
+	}
+
+	css::selector::type_e css::selector::type() const
+	{
+		return iType;
+	}
+
+	css::declaration::declaration()
+	{
+	}
+
+	css::css(const std::string& aStyleSheet) : 
+		css(std::istringstream(aStyleSheet))
+	{
+	}
+
+	namespace
+	{
+		enum class token
+		{
+			Comment,
+			Whitespace,
+			Comma,
+			Period,
+			Colon,
+			DoubleColon, 
+			DoubleQuote,
+			SingleQuote,
+			Backslash,
+			Multiply,
+			Divide,
+			Integer,
+			Float,
+			String,
+			Symbol,
+			PropertyColor,
+			PropertyBackground,
+		};
+
+		typedef neolib::lexer_atom<token> lexer_atom;
+		typedef neolib::lexer_rule<token> lexer_rule;
+		lexer_rule sLexerRules[] =
+		{
+			{{ ',', token::Comma }},
+			{{ '.', token::Period }},
+			{{ '/', token::Divide }},
+			{{ '*', token::Multiply }},
+			{{ ' ', token::Whitespace }}, {{ '\t', token::Whitespace }}, {{ '\r', token::Whitespace }}, {{ '\n', token::Whitespace }},
+			{{ ':', token::Colon }},
+			{{ token::Colon, token::Colon, token::DoubleColon }},
+			{{ '\\', token::Backslash }},
+			{{ token::Divide, token::Multiply, neolib::token_push(token::Comment) }},
+			{{ token::Comment, token::Multiply, token::Divide, neolib::token_pop(token::Comment) }},
+			{{ neolib::token_range<'0', '9'>, token::Integer }},
+			{{ token::Integer, token::Integer, token::Integer }},
+			{{ token::Integer, token::Period, token::Integer, token::Float }},
+			{{ token::Float, token::Integer, token::Float }},
+			{{ neolib::token_range<'A', 'Z'>, token::Symbol }},
+			{{ neolib::token_range<'a', 'z'>, token::Symbol }},
+			{{ token::Symbol, token::Symbol, token::Symbol }},
+			{{ token::Symbol, token::Integer, token::Symbol }},
+			{{ '"', token::DoubleQuote }},
+			{{ '\'', token::SingleQuote }},
+			{{ token::DoubleQuote, token::DoubleQuote, neolib::token_eat_left(neolib::token_eat_right(token::String)) }}, // empty string
+			{{ token::DoubleQuote, neolib::token_not(token::DoubleQuote), neolib::token_eat_left(neolib::token_push(token::String)) }},
+			{{ token::String, token::Backslash, neolib::token_any(), neolib::token_eat_middle(token::String) }},
+			{{ token::String, token::token_not(token::DoubleQuote), token::String }},
+			{{ token::String, token::DoubleQuote, neolib::token_pop(token::String) }},
+			{{ "color"s, token::PropertyColor }},
+			{{ "background"s, token::PropertyBackground }}
+		};
+	}
+
+	css::css(const std::istream& aStyleSheet)
+	{
+		neolib::lexer<token> lexer{ aStyleSheet };
+	}
+
+	void css::accept(i_visitor& aVisitor) const
+	{
+	}
+
+	const css::rule_list& css::rules() const
+	{
+		return iRules;
+	}
+
+	std::string css::to_string() const
+	{
+	}
+}
