5883854d5cabc5879336851df6c7881631b4e258
Leigh Johnston <leigh@i42.co.uk>
2018-02-27 00:46:14 +0000

    Font picker dialog.

--- src/gui/dialog/font_dialog.cpp
+++ src/gui/dialog/font_dialog.cpp
@@ -19,9 +19,7 @@
 
 #include <neogfx/neogfx.hpp>
 #include <neogfx/app/app.hpp>
-#include <neogfx/gui/widget/item_model.hpp>
 #include <neogfx/gui/widget/item_presentation_model.hpp>
-#include <neogfx/gui/widget/item_selection_model.hpp>
 #include <neogfx/gui/dialog/font_dialog.hpp>
 
 namespace neogfx
@@ -122,16 +120,13 @@ namespace neogfx
 	{
 		client_layout().set_size_policy(neogfx::size_policy{ neogfx::size_policy::Minimum, neogfx::size_policy::Maximum });
 
-		i_layout::debug = client_layout().parent();
+		iSample.set_fixed_size(size{ 160.0, 48.0 });
 
 		set_standard_layout(16.0);
 		button_box().add_button(standard_button::Ok);
 		button_box().add_button(standard_button::Cancel);
 
-		auto familyModel = std::make_shared<item_model>();
-		iFamilyPicker.set_model(familyModel);
 		iFamilyPicker.set_presentation_model(std::make_shared<family_picker_presentation_model>());
-		iFamilyPicker.set_selection_model(std::make_shared<item_selection_model>());
 
 		iFamilyPicker.selection_changed([this](optional_item_model_index aIndex)
 		{
@@ -142,21 +137,13 @@ namespace neogfx
 		uint32_t currentIndex = 0;
 		for (uint32_t fi = 0; fi < fm.font_family_count(); ++fi)
 		{
-			familyModel->insert_item(item_model_index{ fi }, fm.font_family(fi));
+			iFamilyPicker.model().insert_item(item_model_index{ fi }, fm.font_family(fi));
 			if (fm.font_family(fi) == app::instance().current_style().font_info().family_name())
 				currentIndex = fi;
 		}
 
-		iStylePicker.set_model(std::make_shared<item_model>());
-		iStylePicker.set_presentation_model(std::make_shared<item_presentation_model>());
-		iStylePicker.set_selection_model(std::make_shared<item_selection_model>());
-
-		auto sizeModel = std::make_shared<item_model>();
-		iSizePicker.set_model(sizeModel);
 		for (auto sz : { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 })
-			sizeModel->insert_item(item_model_index{ sizeModel->rows() }, sz);
-		iSizePicker.set_presentation_model(std::make_shared<item_presentation_model>());
-		iSizePicker.set_selection_model(std::make_shared<item_selection_model>());
+			iSizePicker.model().insert_item(item_model_index{ iSizePicker.model().rows() }, sz);
 
 		update_widgets(*this);
 
@@ -177,7 +164,5 @@ namespace neogfx
 				});
 		else
 			iSample.set_font(optional_font{});
-
-		layout_items();
 	}
 }
