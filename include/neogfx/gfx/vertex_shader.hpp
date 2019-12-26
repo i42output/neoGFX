8b00f19bf1ff19707bc7ae17acec8581b9e46cc6
Leigh Johnston <leigh@i42.co.uk>
2019-12-26 20:46:55 +0000

    GPU shader rework.

--- include/neogfx/gfx/vertex_shader.hpp
+++ include/neogfx/gfx/vertex_shader.hpp
@@ -158,8 +158,7 @@ namespace neogfx
                 {
                     "void standard_vertex_shader(inout vec3 coord, inout vec4 color)\n"
                     "{\n"
-                    "    coord = (uProjectionMatrix * (uTransformationMatrix * vec4(coord, 1.0))).xyz;\n"
-                    "    gl_Position = vec4(coord, 1.0);\n"
+                    "    gl_Position = vec4((uProjectionMatrix * (uTransformationMatrix * vec4(coord, 1.0))).xyz, 1.0);\n"
                     "}\n"_s
                 };
                 aOutput += code;
