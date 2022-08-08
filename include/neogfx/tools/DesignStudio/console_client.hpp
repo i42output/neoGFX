dc1101d6a211accc0583bf7f692d7035b8e2eafa
Leigh Johnston <leigh@i42.co.uk>
2022-08-09 00:15:07 +0100

    source file reorganisation

---
+++ include/neogfx/tools/DesignStudio/console_client.hpp
@@ -0,0 +1,88 @@
+// console_client.hpp
+/*
+  neoGFX Design Studio
+  Copyright(C) 2022 Leigh Johnston
+  
+  This program is free software: you can redistribute it and / or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <neogfx/neogfx.hpp>
+
+namespace neogfx::DesignStudio
+{
+    class i_console_client
+    {
+    public:
+        declare_event(output, std::string const&)
+    public:
+        virtual ~i_console_client() = default;
+    public:
+        virtual void start() = 0;
+        virtual void resize_window(std::uint16_t aWidth, std::uint16_t aHeight) = 0;
+        virtual void input(std::string const& aText) = 0;
+    };
+
+    class console_client : public i_console_client, public neolib::lifetime<>
+    {
+    public:
+        define_declared_event(Output, output, std::string const&)
+    };
+
+    class console : public console_client
+    {
+    public:
+        define_event(Command, command, std::string const&)
+    public:
+        console()
+        {
+        }
+    public:
+        void start() final
+        {
+            output().trigger(">");
+        }
+        void resize_window(std::uint16_t aWidth, std::uint16_t aHeight) final
+        {
+        }
+        void input(std::string const& aText) final
+        {
+            destroyed_flag destroyed{ *this };
+
+            if (aText == "\x7F")
+            {
+                output().trigger("\b\x1B[K");
+                if (!iBuffer.empty())
+                    iBuffer.pop_back();
+                return;
+            }
+
+            output().trigger(aText);
+            iBuffer += aText;
+            std::size_t next;
+            while (!destroyed && (next = iBuffer.find("\r\n")) != std::string::npos)
+            {
+                auto const nextCommand = iBuffer.substr(0, next);
+                iBuffer = iBuffer.substr(next + 2);
+                if (!nextCommand.empty())
+                    command().trigger(nextCommand);
+                if (!destroyed)
+                    output().trigger(">");
+            }
+        }
+    private:
+        std::string iBuffer;
+    };
+}
