40e07dcd693d9ed88317209d25aedf84f63c6766
Leigh Johnston <leigh@i42.co.uk>
2022-08-09 21:45:37 +0100

    neos

--- tools/DesignStudio/element_libraries/neos/src/library.cpp
+++ tools/DesignStudio/element_libraries/neos/src/library.cpp
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <neogfx/tools/DesignStudio/i_project.hpp>
 #include <neogfx/tools/DesignStudio/element.hpp>
 #include <neogfx/tools/DesignStudio/console_client.hpp>
+#include <neos/context.hpp>
 #include "library.hpp"
 
 namespace neogfx::DesignStudio
@@ -35,7 +36,156 @@ namespace neogfx::DesignStudio
         neos_session() :
             console{ "neos" }
         {
+            command([&](std::string const& aCommand)
+            {
+                process_command(aCommand);
+            });
         }
+    private:
+        bool process_command(std::string const& aCommand)
+        {
+            auto output_compilation_time = [&]()
+            {
+                cout << "Compilation time: "s <<
+                    std::chrono::duration_cast<std::chrono::microseconds>(iContext.compiler().end_time() - iContext.compiler().start_time()).count() / 1000.0 << "ms" << std::endl;
+                output().trigger(cout.str());
+            };
+
+            auto previousCurrentPath = std::filesystem::current_path();
+            std::filesystem::current_path(std::filesystem::path{ std::getenv("DevDirNeos") } / "languages");
+            std::vector<std::pair<std::string::const_iterator, std::string::const_iterator>> words;
+            const std::string delimeters{ " " };
+            neolib::tokens(aCommand.cbegin(), aCommand.cend(), delimeters.cbegin(), delimeters.cend(), words);
+            if (words.empty())
+                return true;
+            const std::string command{ words[0].first, words[0].second };
+            if (command.empty())
+                return true;
+            std::string parameters{ words.size() > 1 ? words[1].first : aCommand.cend(), aCommand.cend() };
+            try
+            {
+                if (iInteractive)
+                    iContext.evaluate(aCommand);
+                else if (command == "?" || command == "h" || command == "help")
+                {
+                    cout << "h(elp)\n"
+                        << "s(chema) <path to language schema>       Load language schema\n"
+                        << "l(oad) <path to program>                 Load program\n"
+                        << "list                                     List program\n"
+                        << "c(ompile)                                Compile program\n"
+                        << "r(un)                                    Run program\n"
+                        << "![<expression>]                          Evaluate expression (enter interactive mode if expression omitted)\n"
+                        << ":<input>                                 Input (as stdin)\n"
+                        << "q(uit)                                   Quit neos\n"
+                        << "lc                                       List loaded concept libraries\n"
+                        << "t(race) <0|1|2|3|4|5> [<filter>]         Compiler trace\n"
+                        << "m(etrics)                                Display metrics for running programs\n"
+                        << std::flush;
+                }
+                else if (command == "s" || command == "schema")
+                {
+                    iContext.load_schema(parameters);
+                    cout << "Language: " << iContext.schema().meta().description + "\nVersion: " + iContext.schema().meta().version << std::endl;
+                    if (!iContext.schema().meta().copyright.empty())
+                        cout << iContext.schema().meta().copyright << std::endl;
+                }
+                else if (command == "l" || command == "load")
+                {
+                    iContext.load_program(parameters);
+                    cout << "Program: " << parameters << std::endl;
+                }
+                else if (command == "c" || command == "compile")
+                {
+                    iContext.compile_program();
+                    output_compilation_time();
+                }
+                else if (command == "list")
+                {
+                    for (auto const& tu : iContext.program().translationUnits)
+                        for (auto const& f : tu.fragments)
+                        {
+                            if (f.source_file_path() != std::nullopt)
+                                cout << "File '" << *f.source_file_path() << "':" << std::endl;
+                            cout << f.source() << std::endl;
+                        }
+                }
+                else if (command == "r" || command == "run")
+                    iContext.run();
+                else if (command[0] == '!')
+                {
+                    if (!iContext.schema_loaded())
+                        throw std::runtime_error("no schema loaded");
+                    std::string expression(std::next(words[0].first), aCommand.cend());
+                    if (!expression.empty())
+                    {
+                        iContext.evaluate(expression);
+                        output_compilation_time();
+                    }
+                    else
+                        iInteractive = true;
+                }
+                else if (command == "lc")
+                {
+                    auto print_concept_library = [&](auto const& self, const neos::language::i_concept_library& aConceptLibrary, uint32_t aDepth = 0) -> void
+                    {
+                        cout << std::string(aDepth * 2, ' ') << "[" << aConceptLibrary.name() << "]";
+                        if (aDepth == 0)
+                            cout << " (" << aConceptLibrary.uri() << ")";
+                        cout << std::endl;
+                        for (auto const& conceptLibrary : aConceptLibrary.sublibraries())
+                            self(self, *conceptLibrary.second(), aDepth + 1);
+                    };
+                    for (auto const& conceptLibrary : iContext.concept_libraries())
+                        if (conceptLibrary.second()->depth() == 0)
+                            print_concept_library(print_concept_library, *conceptLibrary.second());
+                }
+                else if (command == "t" || command == "trace")
+                {
+                    if (words.size() >= 2)
+                        iContext.compiler().set_trace(
+                            boost::lexical_cast<uint32_t>(std::string{ words[1].first, words[1].second }),
+                            words.size() > 2 ? std::string{ words[2].first, words[2].second } : std::optional<std::string>{});
+                    else
+                        throw std::runtime_error("invalid command argument(s)");
+                }
+                else if (command == "m" || command == "metrics")
+                    cout << iContext.metrics();
+                else if (command == "q" || command == "quit")
+                    return false;
+                else
+                    throw std::runtime_error("unknown command '" + command + "'");
+            }
+            catch (const neos::language::schema::unresolved_references& e)
+            {
+                for (auto const& r : e.references)
+                    cerr << "Error: " << iContext.schema_source().to_error_text(*r.node, "unresolved schema reference '" + r.symbol + "'") << std::endl;
+            }
+            catch (const neos::context::warning& e)
+            {
+                output_compilation_time();
+                cerr << "Warning: " << e.what() << std::endl;
+            }
+            catch (const std::exception& e)
+            {
+                cerr << "Error: " << e.what() << std::endl;
+            }
+            catch (...)
+            {
+                cerr << "Unknown error" << std::endl;
+            }
+            output().trigger(boost::replace_all_copy(cerr.str(), "\n", "\r\n"));
+            output().trigger(boost::replace_all_copy(cout.str(), "\n", "\r\n"));
+            cerr.str({});
+            cout.str({});
+            std::filesystem::current_path(previousCurrentPath);
+
+            return true;
+        }
+    private:
+        neos::context iContext;
+        bool iInteractive;
+        std::ostringstream cout;
+        std::ostringstream cerr;
     };
 
     neos_element_library::neos_element_library(neolib::i_application& aApplication, std::string const& aPluginPath) :
