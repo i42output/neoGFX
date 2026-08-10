6536c507f17f2594edc8de8d846059d728f5241c
Leigh Johnston <leigh@i42.co.uk>
2026-08-10 18:32:01 +0100

    ECS text

--- include/neogfx/game/text_mesh.hpp
+++ include/neogfx/game/text_mesh.hpp
@@ -22,6 +22,8 @@
 
 #include <neogfx/gfx/primitives.hpp>
 #include <neogfx/gfx/i_graphics_context.hpp>
+#include <neogfx/gfx/i_rendering_engine.hpp>
+#include <neogfx/gfx/text/i_font_manager.hpp>
 #include <neogfx/game/component.hpp>
 #include <neogfx/game/entity.hpp>
 #include <neogfx/game/renderable_entity_archetype.hpp>
@@ -143,32 +145,52 @@ namespace neogfx::game
                 auto multilineGlyphText = aGc.to_multiline_glyph_text(aData.text, service<i_font_manager>().font_from_id(aData.font->id.cookie()), aData.extents.x, aData.alignment);
                 for (auto const& line : multilineGlyphText.lines)
                 {
-                    auto mesh_line = [&](const vec3f& aOffset, const material& aMaterial) 
+                    auto mesh_line = [&](const vec3f& aOffset, const material& aMaterial, bool aEffect) 
                     {
                         auto const glyphs = std::ranges::subrange(std::next(multilineGlyphText.glyphText.cbegin(), line.begin), std::next(multilineGlyphText.glyphText.cbegin(), line.end));
                         auto const pos = aOffset + line.bbox[0] - vec3f{ glyphs.begin()->cell[0] };
                         for (auto const& glyphChar : glyphs)
                         {
-                            if (!is_whitespace(glyphChar))
+                            if (is_whitespace(glyphChar))
+                                continue;
+                            else if (!is_emoji(glyphChar))
                             {
                                 auto const& glyphTexture = multilineGlyphText.glyphText.glyph(glyphChar);
                                 auto& patch = *add_patch(*mf.mesh, mr, pos + vec3f{ glyphChar.cell[0] } + quadf{ glyphChar.shape[0], glyphChar.shape[1], glyphChar.shape[2], glyphChar.shape[3] }, glyphTexture.texture());
-                                patch.material = material{ aMaterial.color, aMaterial.gradient, aMaterial.sharedTexture, patch.material.texture, aMaterial.shaderEffect };
+                                patch.material = material{
+                                    aMaterial.color,
+                                    aMaterial.gradient,
+                                    aMaterial.sharedTexture,
+                                    patch.material.texture,
+                                    aMaterial.shaderEffect };
+                            }
+                            else if (!aEffect)
+                            {
+                                auto const& emojiAtlas = service<i_rendering_engine>().font_manager().emoji_atlas();
+                                auto const& emojiTexture = emojiAtlas.emoji_texture(glyphChar.value).as_sub_texture();
+                                auto& patch = *add_patch(*mf.mesh, mr, pos + vec3f{ glyphChar.cell[0] } + quadf{ glyphChar.shape[0], glyphChar.shape[1], glyphChar.shape[2], glyphChar.shape[3] }, emojiTexture);
+                                patch.material = material{ 
+                                    {},
+                                    {},
+                                    aMaterial.sharedTexture, 
+                                    patch.material.texture, 
+                                    {} };
                             }
                         }
                     };
                     switch(aData.textEffect)
                     {
                     case text_effect_type::Outline:
+                        // @todo - use FT outlines
                         for (float oy = -aData.textEffectWidth; oy <= aData.textEffectWidth; ++oy)
                             for (float ox = -aData.textEffectWidth; ox <= aData.textEffectWidth; ++ox)
-                                mesh_line(vec2f{ ox, oy }, aData.textEffectMaterial);
+                                mesh_line(vec2f{ ox, oy }, aData.textEffectMaterial, true);
                         break;
                     default:
-                        // todo
+                        // @todo
                         break;
                     }
-                    mesh_line(vec2f{}, aData.ink);
+                    mesh_line(vec2f{}, aData.ink, false);
                 }
             }
         };
